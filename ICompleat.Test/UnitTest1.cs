3afebdec2543d1a724344def077c879fedb63ccd
Oscar Davies <oscar.william.davies@outlook.com>
2022-02-16 10:57:37 +0000

    Custom Fields

--- ICompleat.Test/UnitTest1.cs
+++ ICompleat.Test/UnitTest1.cs
@@ -8,9 +8,19 @@ public class UnitTest1
     #region Methods
 
     [Fact]
-    public async void RequestsWork()
+    public async void CustomFieldsWork()
     {
-        var t = await Objects.Supplier.GetSuppliersUntillAllAsync();
+        var t = await Objects.CustomFields.GetCustomFieldsAsync();
+        Assert.NotNull(t);
+
+        await t[0].LoadFull();
+        Assert.NotNull(t[0]);
+    }
+
+    [Fact]
+    public async void SuppliersWork()
+    {
+        var t = await Objects.Supplier.GetSuppliersAsync();
         Assert.NotNull(t);
     }
 
---
+++ ICompleat/Objects/CustomFields.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ICompleat.Objects
+{
+    public class CustomFields : JsonObject
+    {
+        #region Properties
+
+        public string? fieldid
+        {
+            get { return json.GetProperty("Id").GetString(); }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public static async Task<CustomFields> GetCustomFieldAsync(string fieldid)
+        {
+            var d = await Execucte($"api/customfield/{Config._instance.tenantId}/{Config._instance.companyId}/{fieldid}");
+
+            return new CustomFields() { json = d.GetProperty("CustomField") };
+        }
+
+        public static async Task<CustomFields[]> GetCustomFieldsAsync()
+        {
+            var d = await Execucte($"api/customfields/{Config._instance.tenantId}/{Config._instance.companyId}");
+
+            List<CustomFields> s = new List<CustomFields>();
+            foreach (JsonElement e in d.GetProperty("CustomFields").EnumerateArray())
+            {
+                s.Add(new CustomFields() { json = e });
+            }
+            return s.ToArray();
+        }
+
+        public async Task LoadFull()
+        {
+            if (fieldid?.Length > 0)
+            {
+                json = (await GetCustomFieldAsync(fieldid)).json;
+            }
+            return;
+        }
+
+        #endregion Methods
+    }
+}
