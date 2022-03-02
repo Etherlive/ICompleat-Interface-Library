8a29457ebe7ce91e1f4ead488086727598794b4b
Oscar Davies <oscar.william.davies@outlook.com>
2022-03-02 10:26:46 +0000

    Add Properties to Suppliers

--- ICompleat/Objects/Suppliers.cs
+++ ICompleat/Objects/Suppliers.cs
@@ -8,6 +8,85 @@ namespace ICompleat.Objects
 {
     public class Supplier : JsonObject
     {
+        #region Properties
+
+        public string AddressLine1
+        {
+            get { return json.TryGetProperty("AddressLine1", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string AddressLine2
+        {
+            get { return json.TryGetProperty("AddressLine2", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string BankAccountNumber
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string BankName
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string BankSortCode
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string Code
+        {
+            get { return json.TryGetProperty("Code", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string Country
+        {
+            get { return json.TryGetProperty("Country", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string Email
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string IsoCode
+        {
+            get { return json.TryGetProperty("IsoCode", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string Name
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string PaymentDueDays
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string PaymentTermsType
+        {
+            get { return json.TryGetProperty("Name", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string PostcodeOrZip
+        {
+            get { return json.TryGetProperty("PostcodeOrZip", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string StateOrCounty
+        {
+            get { return json.TryGetProperty("StateOrCounty", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        public string Telephone
+        {
+            get { return json.TryGetProperty("Telephone", out JsonElement e) ? e.GetString() : ""; }
+        }
+
+        #endregion Properties
+
         #region Methods
 
         public static async Task<Supplier[]> GetSuppliersAsync(int page = 1)
